ee0931b8f19aa6ed920084f4f573f435db674bf6
kjuiop <copying_y@naver.com>
2022-03-09 22:05:29 +0900

    Initial commit

---
+++ src/main/java/com/gig/spring_cloud_discovery/SpringCloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.gig.spring_cloud_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gig/spring_cloud_discovery/SpringCloudDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gig.spring_cloud_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
