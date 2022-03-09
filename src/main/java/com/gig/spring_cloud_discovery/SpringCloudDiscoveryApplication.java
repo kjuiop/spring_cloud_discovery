31a5db01448eb988595c5799276c97e167892140
kjuiop <copying_y@naver.com>
2022-03-09 22:14:35 +0900

    spring eureka server start

--- src/main/java/com/gig/spring_cloud_discovery/SpringCloudDiscoveryApplication.java
+++ src/main/java/com/gig/spring_cloud_discovery/SpringCloudDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.gig.spring_cloud_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringCloudDiscoveryApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,18 @@ public class SpringCloudDiscoveryApplication {
     }
 
 }
+
+/**
+ * @EnaleEurekaServer
+ * 유레카 서버의 자격으로 등록해주는 어노테이션
+ *
+ * 유레카 설정파일을 application.yml 에 등록한다.
+ *
+ * eureka:
+ *  client:
+ *   register-with-eureka: false
+ *   fetch-registry: false
+ *
+ * 기본값이 True 로 서버를 eureka 에 client 로 등록하기 때문에
+ * 자기 자신을 서버에 등록하지 않는 것으로 설정
+ */
